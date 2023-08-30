7bfcbdda9ee2d725bea7bfee7d3a6cff7dc35165
Phùng Tiến Dũng <tiendung0325@gmail.com>
2023-08-30 16:56:24 +0000

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# A Step-by-Step Guide to Building Your Own Blynk Alternative With Node.js, Socket.io, and ESP8266 Microcontroller
+# create remote desktop switch using nodemcu esp8266
 
-![](https://images.unsplash.com/photo-1677092590812-78e7db4900d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=80)
+![](https://blog.mecsu.vn/wp-content/uploads/2022/02/nodemcu-esp8266-1.jpg)
 
-Full tutorial at [Instructables](https://www.instructables.com/Build-Your-Own-IoT-Platform-Using-Nodejs-and-ESP82/) or [hackster.io](https://www.hackster.io/B45i/build-your-own-iot-platform-using-node-js-and-esp8266-c9e501)
+
--- src/app.js
+++ src/app.js
@@ -9,22 +9,17 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
 app.use(express.static('src/ui'));
-
-let buttonState = false;
-
+let mac = "";
 io.on('connection', socket => {
     console.log('New Connection');
-
-    io.to(socket.id).emit('buttonState', buttonState);
-
+    io.to(socket.id).emit('tidvn', mac);
     socket.on('disconnect', () => {
         console.log('Disconnected');
     });
-
-    socket.on('buttonState', value => {
-        console.log('buttonState:', value);
-        buttonState = value;
-        socket.broadcast.emit('buttonState', value);
+    
+    socket.on('tidvn', data => {
+        mac = data;
+        socket.broadcast.emit('tidvn', data);
     });
 });
 
